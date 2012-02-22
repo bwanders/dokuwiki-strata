df13cb31bf27fdf42711974f7dd47a3c80db775b
Brend Wanders <b.wanders@utwente.nl>
2012-02-22 15:01:24 +0100

    Adds type hints.

--- syntax/entry.php
+++ syntax/entry.php
@@ -44,21 +44,21 @@ class syntax_plugin_stratabasic_entry extends DokuWiki_Syntax_Plugin {
         preg_match('/^<data( [_a-zA-Z0-9 ]+)?(?: ?#([^>]*?))?>/', array_shift($lines), $header);
 
         foreach(preg_split('/\s+/',trim($header[1])) as $class) {
-            $result['triples'][] = array('key'=>'class','value'=>$class,'type'=>null);
+            $result['triples'][] = array('key'=>'class','value'=>$class,'type'=>null, 'hint'=>null);
         }
 
         $result['entry'] = $header[2];
 
         foreach($lines as $line) {
             if($line == '</data>') break;
-            if(preg_match('/^([-a-zA-Z0-9 ]+)(?:_([a-z0-9]+))?(\*)?:(.*)$/',$line,$parts)) {
-                if($parts[3] == '*') {
-                    $values = array_map('trim',explode(',',$parts[4]));
+            if(preg_match('/^([-a-zA-Z0-9 ]+)(?:_([a-z0-9]+)(?:\(([^)]+)\))?)?(\*)?:(.*)$/',$line,$parts)) {
+                if($parts[4] == '*') {
+                    $values = array_map('trim',explode(',',$parts[5]));
                 } else {
-                    $values = array(trim($parts[4]));
+                    $values = array(trim($parts[5]));
                 }
                 foreach($values as $v) {
-                    $result['triples'][] = array('key'=>$parts[1],'value'=>$v,'type'=>$parts[2]);
+                    $result['triples'][] = array('key'=>$parts[1],'value'=>$v,'type'=>$parts[2],'hint'=>$parts[3]);
                 }
             } else {
                 msg('I don\'t understand data entry \''.htmlentities($line).'\'.', -1);
