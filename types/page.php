2898a2aea9e31deabdb6c1d3edea22658b4e5dc4
Brend Wanders <b.wanders@utwente.nl>
2012-02-24 17:11:47 +0100

    Adds the 'page' type.

---
+++ types/page.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Brend Wanders <b.wanders@utwente.nl>
+ */
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+/**
+ * The page link type.
+ */
+class plugin_strata_type_page extends plugin_strata_type {
+    function normalize($value, $hint) {
+        global $ID;
+
+        // resolve page id with current location as base
+        resolve_pageid(getNS($ID),$value,$exists);
+
+        return $value;
+    }
+
+    function render($mode, &$R, $value, $hint) {
+        // render internal link
+        // (':' is prepended to make sure we use an absolute pagename,
+        // internallink resolves page names, but the name is already resolved.)
+        $R->internallink(':'.$value);
+    }
+
+    function getInfo() {
+        return array(
+            'desc'=>'The value is used as a wiki page name and linked if appropriate.'
+        );
+    }
+}
