7c7896572c317c5285543d750f33916222b5538c
Brend Wanders <b.wanders@utwente.nl>
2012-02-29 17:16:38 +0100

    Adds debug view of query results.

--- syntax/select.php
+++ syntax/select.php
@@ -86,11 +86,12 @@ class syntax_plugin_stratabasic_select extends DokuWiki_Syntax_Plugin {
     }
 
     function render($mode, &$R, $data) {
-        global $ID;
-
+        $result = $this->_triples->queryRelations($data['query']);
 
         if($mode == 'xhtml') {
-            $R->preformatted(print_r($data,1));
+            $R->preformatted(print_r($data['fields'],1));
+            $R->code($result['sql'],'sql');
+            $R->preformatted(print_r($result['literals'],1));
             return true;
         } elseif($mode == 'metadata') {
             return false;
