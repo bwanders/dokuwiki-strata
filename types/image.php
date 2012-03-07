f5d5d44cbe0dde622a5ca56f3efc9efca2dce603
Brend Wanders <b.wanders@utwente.nl>
2012-03-07 15:34:41 +0100

    Adds image type.

---
+++ types/image.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Brend Wanders <b.wanders@utwente.nl>
+ */
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+/**
+ * The image type.
+ */
+class plugin_strata_type_image extends plugin_strata_type {
+    function normalize($value, $hint) {
+        global $ID;
+
+        // resolve page id with respect to selected base
+        resolve_mediaid(getNS($ID),$value,$exists);
+
+        return $value;
+    }
+
+    function render($mode, &$R, &$T, $value, $hint) {
+        if($hint==null) {
+            $width = null;
+        } else {
+            $width = (int)$hint;
+        }
+
+        // render internal media
+        // (':' is prepended to make sure we use an absolute pagename,
+        // internalmedia resolves media ids, but the name is already resolved.)
+        $R->internalmedia(':'.$value,null,null,$width);
+    }
+
+    function getInfo() {
+        return array(
+            'desc'=>'The value is used as a wiki media id and shown if appropriate. The optional hint is treated as the width to scale the image to.'
+        );
+    }
+}
