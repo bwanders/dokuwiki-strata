75ca72c3ab976178ff47d5dbd4171ccd7e5c069f
Steven te Brinke <s.tebrinke@utwente.nl>
2012-03-20 13:38:58 +0000

    Adds meta test for full strata functionality.

---
+++ _test/full.group.php
@@ -0,0 +1,11 @@
+<?php
+require_once(DOKU_INC.'_test/lib/unittest.php');
+class full_group_test extends Doku_GroupTest {
+    function full_group_test() {
+        $dir = dirname(__FILE__).'/';
+        $this->addTestFile($dir . 'types.test.php');
+        $this->addTestFile($dir . 'storage.group.php');
+        $this->addTestFile($dir . 'query.test.php');
+    }
+}
+
