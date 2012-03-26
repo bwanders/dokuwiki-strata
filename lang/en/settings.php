0ce735670eee288e6e3002d42a1c9ad38aba6ae7
Brend Wanders <b.wanders@utwente.nl>
2012-03-26 14:55:47 +0200

    Improves the label of the configuration options.

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -3,6 +3,6 @@
 $lang['default_type'] = 'Default value type';
 $lang['predicate_type'] = 'Default predicate type';
 $lang['default_dsn'] = 'Default database source name (used by PDO)';
-$lang['isa_key'] = 'The key used for the \'is a\' predicate';
-$lang['title_key'] = 'The key used for the entry title predicate';
+$lang['isa_key'] = 'The name of the \'is a\' relation';
+$lang['title_key'] = 'The name of the \'entry title\' relation';
 $lang['debug'] = 'Enable debug information? (Warning: enabling debug information may expose confidential information)';
