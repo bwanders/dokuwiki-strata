28c19ea9919b843018c4674188b0e9df5596cefa
Brend Wanders <b.wanders@utwente.nl>
2012-05-04 17:15:27 +0200

    Adds strict hint to numeric aggregators.

--- aggregates/min.php
+++ aggregates/min.php
@@ -11,13 +11,15 @@ if(!defined('DOKU_INC')) die('Meh.');
  */
 class plugin_strata_aggregate_min extends plugin_strata_aggregate {
     function aggregate($values, $hint = null) {
+        if($hint == 'strict') $values = array_filter($values, 'is_numeric');
         if(empty($values)) return array();
         return array(min($values));
     }
 
     function getInfo() {
         return array(
-            'desc'=>'Returns the minimum value. Any item that does not have a clear numeric value (i.e. starts with a number) is counted as 0.',
+            'desc'=>'Returns the minimum value. Any item that does not have a clear numeric value (i.e. starts with a number) is counted as 0. If the \'strict\' hint is used, values that are not strictly numeric (i.e. contains only a number) are ignored.',
+            'hint'=>'\'strict\' to ignore non-numeric values',
             'tags'=>array('numeric')
         );
     }
