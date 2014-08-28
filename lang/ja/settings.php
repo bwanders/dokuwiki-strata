cd49a6c6d2e67ac073e20ec500a4eb7b5c28a92d
Hideaki SAWADA <chuno@live.jp>
2014-08-28 16:01:56 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*
+* @author Hideaki SAWADA <chuno@live.jp>
+*/
+$lang['default_type'] = 'デフォルトの値タイプ';
+$lang['predicate_type'] = 'デフォルト predicate タイプ';
+$lang['default_dsn'] = 'デフォルトデータベースソース名（PDO が使用する）';
+$lang['isa_key'] = '\'is a\' 関係の名前';
+$lang['title_key'] = '\'entry title\' 関係の名前';
+$lang['debug'] = 'デバッグ情報の有効化（警告：有効にすると、機密情報を漏洩する可能性がある）';
+
+$lang['enable_entry'] = 'データ入力構文の有効化';
+
